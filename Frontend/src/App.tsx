f00bd450d40e21d1bc5abd61f216f7a607d9b306
kv272 <vermakunal177@gmail.com>
2024-07-15 17:04:43 +0530

    integrated UI

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import WordFadeIn from "@/components/magicui/word-fade-in";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+import { BorderBeam } from "@/components/magicui/border-beam";
 
+
+function App() {  
   return (
     <>
-    
+    <WordFadeIn words="Hello bro Don't worry. Let's solve the problem"></WordFadeIn>
     </>
   )
 }
